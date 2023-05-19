4d81cf762e4c0949afeab85dc40bebac2470e5b5
aaaaaaaaaaaaaaaaasaaaasza <jomama420696969ligma@gmail.com>
2023-05-19 19:27:36 -0300

    first commit

---
+++ src/main/java/com/io/demo/DkApplication.java
@@ -0,0 +1,13 @@
+package com.io.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/demo/DkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
